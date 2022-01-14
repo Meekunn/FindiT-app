43ee7cabd9748b6ef233290ca4217d7225b8c8ac
Tomi <orofinadedamola@gmail.com>
2022-01-14 04:28:17 +0100

    Welcome Page Design completed

---
+++ src/pages/welcome.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import '../style/welcome.scss'
+import { Button } from "@material-ui/core";
+import { Container, Box } from "@material-ui/core";
+
+const Welcome = () =>{
+    return(
+        <div className='main-welcome'>
+            <div className='welcome-box'>
+                <div className='box-content'>
+                    <h1>Welcome to FindiT</h1>
+                    <p>Dedicated to help you improve your relation with lecturers.</p>
+                    <Button variant="contained">Sign up as a lecturer</Button>
+                    <Button variant="outlined">Continue as a Student</Button>
+                </div>
+                <div className='box-blank'>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
